45f16a295c234b920b29aaf37dd9581ac76cdb05
kyweez <auboudier.pro@gmail.com>
2020-05-06 16:46:27 +0200

    Exercice 1.1.1 update (comments)

--- FOAD_ALGO/Part_01/Exercice111.java
+++ FOAD_ALGO/Part_01/Exercice111.java
@@ -14,16 +14,21 @@ public class Exercice111 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		//Variables declaration
 		Scanner sc;
 		double nbr1;
 		double nbr2;
-		sc = new Scanner(System.in);
+		//Program title
 		System.out.println("Average of 2 numbers calculating program");
 		System.out.println("========================================\n");
+		//Start
 		System.out.println("Please insert 2 numbers separated by a space:");
+		sc = new Scanner(System.in);
 		nbr1 = Double.parseDouble(sc.next());
 		nbr2 = Double.parseDouble(sc.next());
+		//Print result
 		System.out.println("The average of " + nbr1 + " and " + nbr2 + " is : " + (nbr1 + nbr2) / 2);
+		//Scanner closing (Do not forget)
 		sc.close();
 	}
 }
