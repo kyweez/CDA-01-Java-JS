bd2674e2b914ae6c290a670a1ec504e548271553
kyweez <auboudier.pro@gmail.com>
2020-05-07 07:48:09 +0200

    Exercise 1.6.0 OK (non optimized algo)

---
+++ FOAD_ALGO/Part_01/Exercice160.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+/**
+ * Read a number and find if it's a prime number.
+ * 
+ * WARNING !!!
+ * This exercise do not check the user input error cases.
+ * We assume that the user will put valid values.
+ * Also, we don't cover the max/min values.
+ */
+
+/**
+ * @author Kyweez
+ */
+public class Exercice160 {
+
+	public static boolean isPrime(int number) {
+		if (number < 0)
+			return false;
+		for (int i = 2; i < number; i++) {
+			if (number % i == 0)
+				return false;
+		}
+		return (true);
+	}
+
+	
+	public static void main(String[] args) {
+		// Variables declaration
+		Scanner sc;
+		int number;
+
+		// Program title
+		System.out.println("Is prime program (non optimized algo)");
+		System.out.println("================\n");
+
+		// ### Start ###//
+		sc = new Scanner(System.in);
+		System.out.println("Please enter a number :");
+		number = sc.nextInt();
+		System.out.println(number + (isPrime(number)?" is prime": " isn't prime"));
+		// Scanner closing (Do not forget)
+		sc.close();
+	}
+}
