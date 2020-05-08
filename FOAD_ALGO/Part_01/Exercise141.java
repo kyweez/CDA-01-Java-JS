15e9b504c1a7d3f4ac8c065c9b69459d0f75bc94
kyweez <auboudier.pro@gmail.com>
2020-05-08 22:35:39 +0200

    Exercise 141, trying a different way

---
+++ FOAD_ALGO/Part_01/Exercise141.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+/**
+ * Read 2 numbers a and b.
+ * Put the contents of a in b and that of b in a.
+ * 
+ * WARNING !!!
+ * This exercise do not check the user input error cases.
+ * We assume that the user will put valid values.
+ * Also, we don't cover the max/min values.
+ */
+
+/**
+ * @author Kyweez
+ */
+public class Exercise141 {
+	
+	public static void swap(Double a, Double b) {
+		Double temp;
+		
+		temp = a;
+		a = b;
+		b = temp;
+	}
+	
+	public static void main(String[] args) {
+		// Variables declaration
+		Scanner sc;
+		Double a;
+		Double b;
+
+		// Program title
+		System.out.println("Values swap program");
+		System.out.println("===================\n");
+
+		// ### Start ###//
+		sc = new Scanner(System.in);
+		System.out.println("Please enter a number a :");
+		a = sc.nextDouble();
+		System.out.println("a = " + a);
+		System.out.println("Please enter a number b :");
+		b = sc.nextDouble();
+		System.out.println("b = " + b);
+		System.out.println("\nExecution of the algorithm...");
+		
+		swap(a, b);
+		System.out.println("Now, a = " + a + " and b = " + b);
+
+		// Scanner closing (Do not forget)
+		sc.close();
+	}
+}
