cc140f48f92a9ec99c900e889270b90c1767872e
kyweez <auboudier.pro@gmail.com>
2020-05-09 11:46:53 +0200

    Exercise 1.7.3 - Let's have fun with ternary operations - OK

---
+++ FOAD_ALGO/Part_01/Exercise173.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+/**
+ * Read a year A.
+ * Determine if year A is a leap year. If A is not divisible by 4, the year is not a leap year.
+ * If A is divisible by 4, the year is a leap year unless A is divisible by 100 and not by 400.
+ * Write a message "Leap" or "No leap" as appropriate.
+ * 
+ * WARNING !!!
+ * This exercise do not check the user input error cases.
+ * We assume that the user will put valid values.
+ * Also, we don't cover the max/min values.
+ */
+
+/**
+ * @author Kyweez
+ */
+public class Exercise173 {
+	
+	public static boolean leapYear(int year) {
+		return (year % 4 == 0 ? (year % 100 == 0 && year % 400 !=0) ? (false) : (true) : (false));
+	} 
+	
+	public static void main(String[] args) {
+		// Variables declaration
+		Scanner sc;
+		int year;
+
+		// Program title
+		System.out.println("Leap year or not ? Let's have fun with ternary operations");
+		System.out.println("=========================================================\n");
+
+		// ### Start ###//
+		sc = new Scanner(System.in);
+		System.out.println("Please insert a year");
+		year = sc.nextInt();
+		if (leapYear(year))
+			System.out.println(year + " is a leap year");
+		else
+			System.out.println(year + " is a non leap year");
+		sc.close();
+	}
+}
