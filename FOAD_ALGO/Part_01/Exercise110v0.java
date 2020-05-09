389815e305e46ea6f200e3cbc135fe1c123a56ac
kyweez <auboudier.pro@gmail.com>
2020-05-09 13:56:10 +0200

    Exercise 1.10v0 - Perfect number program - OK

--- FOAD_ALGO/Part_01/Exercise110v0.java
+++ FOAD_ALGO/Part_01/Exercise110v0.java
@@ -14,23 +14,50 @@ import java.util.Scanner;
  */
 public class Exercise110v0 {
 	
+	//This method returns if a number is perfect
+	public static boolean isPerfect(int number) {
+		return (number == divisorSum(number) ? (true) : (false));
+	}
+	
+	//This method find all divisors of a number add them together
+	public static int divisorSum(int number) {
+		int sum = 0;
+		
+		for (int i = 1; i < number; i++) {
+			if (number%i == 0) {
+				//Test to avoid overflows
+				if (i <= 2147483647)
+					sum += i;
+				else {
+					System.out.println("This program can't handle this number : Out of bounds");
+					return (-1);
+				}
+			}
+		}
+		return (sum);
+	}
+	
 	public static void main(String[] args) {
 		// Variables declaration
 		Scanner sc;
 		int number;
+		int i = 0;
+		int j = 1;
 
 		// Program title
-		System.out.println("Find all divisors");
-		System.out.println("=================\n");
+		System.out.println("Perfect number program");
+		System.out.println("======================\n");
 
 		// ### Start ###//
 		sc = new Scanner(System.in);
-		System.out.println("Please enter a number :");
+		System.out.println("Please enter a number of the perfect number you want to see :");
 		number = sc.nextInt();
-		for (int i = 2; i < number; i++) {
-			if (number%i == 0) {
-				System.out.println(i);
+		while (i < number) {
+			if (isPerfect(j)) {
+				System.out.println(j + " is a perfect number");
+				i++;
 			}
+			j++;
 		}
 		sc.close();
 	}
