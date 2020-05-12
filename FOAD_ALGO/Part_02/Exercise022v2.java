ea7a801c41ee6a849aeb07717b2a220ae37402c8
kyweez <auboudier.pro@gmail.com>
2020-05-13 00:18:37 +0200

    Exercise 2.2.2 - Temperature converter - Bad code... Checking tomorrow!

--- FOAD_ALGO/Part_02/Exercise022v2.java
+++ FOAD_ALGO/Part_02/Exercise022v2.java
@@ -45,6 +45,9 @@ public class Exercise022v2 {
 		// Assignments
 		sc = new Scanner(System.in);
 		pattern = "^(-?[0-9]{1,10})(\\.[0-9]+)?$";
+		value1 = "";
+		value2 = "";
+		unit = "";
 		validUnit = false;
 		while (!validUnit) {
 			System.out.println("Insert a temperature unit (C or F)");
@@ -56,16 +59,46 @@ public class Exercise022v2 {
 					System.out.println("Insert a minimum temperature");
 					value1 = sc.nextLine();
 					if (regexChecker(pattern, value1)) {
-						validMinValue = true;
-						System.out.println("Insert a maximum temperature");
+						if (Double.parseDouble(value1) >= -459.67) {
+							if (Double.parseDouble(value1) <= 5000000) {
+								validMinValue = true;
+								System.out.println("Insert a maximum temperature");
+								value2 = sc.nextLine();
+								if (regexChecker(pattern, value2)) {
+									if (Double.parseDouble(value2) >= -459.67) {
+										if (Double.parseDouble(value2) <= 5000000) {
+											validMaxValue = true;
+										}
+										else {
+											System.out.println("Bad input insert a value between -459.67 and 5000000");
+										}
+									}
+									else {
+										System.out.println("Bad input insert a value between -459.67 and 5000000");
+									}
+
+								}
+								else {
+									System.out.println("Bad input : Insert a valid value ");
+								}
+							}
+							else {
+								System.out.println("Bad input insert a value between -459.67 and 5000000");
+							}
+						}
+						else {
+							System.out.println("Bad input insert a value between -459.67 and 5000000");
+						}
+
 					}
 					else {
-						System.out.println("Bad input insert a value between -459.67 and 5000000");
+						System.out.println("Bad input : Insert a valid value ");
 					}
 				}
 			}
 			System.out.println("Bad input : Insert C or F");
 		}
+		System.out.println(value1 + " " + value2 + " " + unit);
 
 		sc.close();
 	}
