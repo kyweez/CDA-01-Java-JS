3de4bf018ea4d27fb4969ca83b89d62ad8a185c1
kyweez <auboudier.pro@gmail.com>
2020-05-13 16:40:42 +0200

    Exercise 2.2.2 - Temperature converter - Updates (modulating code)

--- FOAD_ALGO/Part_02/Exercise022v2.java
+++ FOAD_ALGO/Part_02/Exercise022v2.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import java.util.regex.*;
 
+import javax.swing.text.StyledEditorKit.BoldAction;
+
 /**
  * The user inputs a numerical value between -459.67 and 5,000,000 followed by the temperature unit:
  * 		▪ C for Celsius
@@ -17,6 +19,46 @@ import java.util.regex.*;
  */
 public class Exercise022v2 {
 
+	public static boolean boundsChecker(String inputValue, double min, double max) {
+		double value = Double.parseDouble(inputValue);
+		if (value >= min && value <= max)
+			return (true);
+		else
+			return (false);
+	}
+	
+	public static double setInputDouble(Scanner sc, String pattern, double[] bound) {
+		boolean validInput = false;
+		String input = "";
+		while (!validInput) {
+			System.out.println("Insert a value between " + bound[0] + " & " + bound[1]);
+			input = sc.nextLine();
+			if (regexChecker(pattern, input)) {
+				if (boundsChecker(input, bound[0], bound[1]))
+					return (Double.parseDouble(input));
+				else
+					System.out.println("Bad input !");
+			}
+			else 
+				System.out.println("Bad input, try again!");
+		}
+		return (Double.parseDouble(input));
+	}
+	
+	public static String setInputString (Scanner sc, String pattern) {
+		boolean validInput = false;
+		String input = "";
+		while (!validInput) {
+			System.out.println("Please insert a unit (C/F)");
+			input = sc.nextLine();
+			if (regexChecker(pattern, input))
+				return (input);
+			else
+				System.out.println("Bad input, try again!");
+		}
+		return (input);
+	}
+	
 	public static boolean regexChecker(String pattern, String input) {
 		Pattern regexPattern = Pattern.compile(pattern);
 		Matcher regexMatcher = regexPattern.matcher(input);
@@ -28,14 +70,15 @@ public class Exercise022v2 {
 	 */
 	public static void main(String[] args) {
 		// Variables declaration
-		Scanner sc;
+		Scanner sc = new Scanner(System.in);;
 		String unit;
-		String pattern;
-		String value1;
-		String value2;
-		boolean validUnit;
-		boolean validMinValue;
-		boolean validMaxValue;
+		String paternUnit = "(^C$)|(^F$)";
+		String patternValue = "^(-?[0-9]{1,10})(\\\\.[0-9]+)?$";
+		double value1;
+		double value2 = 0;
+		double[] bound = {-459.67, 5000000};
+		boolean validValue = true;
+		
 
 		// Program title
 		System.out.println("Temperature converter program");
@@ -43,63 +86,15 @@ public class Exercise022v2 {
 
 		// ### Start ###
 		// Assignments
-		sc = new Scanner(System.in);
-		pattern = "^(-?[0-9]{1,10})(\\.[0-9]+)?$";
-		value1 = "";
-		value2 = "";
-		unit = "";
-		validUnit = false;
-		while (!validUnit) {
-			System.out.println("Insert a temperature unit (C or F)");
-			unit = sc.nextLine();
-			if (regexChecker("(^C$)|(^F$)", unit)) {
-				validUnit = true;
-				validMinValue = false;
-				while (!validMinValue) {
-					System.out.println("Insert a minimum temperature");
-					value1 = sc.nextLine();
-					if (regexChecker(pattern, value1)) {
-						if (Double.parseDouble(value1) >= -459.67) {
-							if (Double.parseDouble(value1) <= 5000000) {
-								validMinValue = true;
-								System.out.println("Insert a maximum temperature");
-								value2 = sc.nextLine();
-								if (regexChecker(pattern, value2)) {
-									if (Double.parseDouble(value2) >= -459.67) {
-										if (Double.parseDouble(value2) <= 5000000) {
-											validMaxValue = true;
-										}
-										else {
-											System.out.println("Bad input insert a value between -459.67 and 5000000");
-										}
-									}
-									else {
-										System.out.println("Bad input insert a value between -459.67 and 5000000");
-									}
-
-								}
-								else {
-									System.out.println("Bad input : Insert a valid value ");
-								}
-							}
-							else {
-								System.out.println("Bad input insert a value between -459.67 and 5000000");
-							}
-						}
-						else {
-							System.out.println("Bad input insert a value between -459.67 and 5000000");
-						}
-
-					}
-					else {
-						System.out.println("Bad input : Insert a valid value ");
-					}
-				}
+		unit = setInputString(sc, paternUnit);
+		value1 = setInputDouble(sc, patternValue, bound);
+		while (validValue) {
+			value2 = setInputDouble(sc, patternValue, bound);
+			if (value1 > value2) {
+				System.out.println("The second value must be higher than the first one !");
 			}
-			System.out.println("Bad input : Insert C or F");
 		}
-		System.out.println(value1 + " " + value2 + " " + unit);
-
+		System.out.println("RESULT = " + value1 +" "+ value2 +" "+ unit);
 		sc.close();
 	}
 }
