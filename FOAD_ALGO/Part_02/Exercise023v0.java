13c7f9a8ea0c381873ee0a3f4c0d66228810aa31
kyweez <auboudier.pro@gmail.com>
2020-05-13 23:27:19 +0200

    Exercise 2.3.0 - Starting programming - Push before sleep

--- FOAD_ALGO/Part_02/Exercise023v0.java
+++ FOAD_ALGO/Part_02/Exercise023v0.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * 
  */
@@ -8,6 +10,28 @@
  */
 public class Exercise023v0 {
 
+	public static boolean compare(byte inputNumber, byte searchedNumber) {
+		if (inputNumber == searchedNumber)
+			return (true);
+		else if (inputNumber < searchedNumber) {
+			System.out.println("It' more\n");
+			return (false);
+		}
+		else {
+			System.out.println("It' less\n");
+			return (false);
+		}
+	}
+	
+	public static int randomByte(int nb) {
+		Random generator;
+		int randomNumber;
+
+		generator = new Random();
+		randomNumber = generator.nextInt(nb + 1);
+		return (randomNumber);
+	}
+	
 	/**
 	 * @param args
 	 */
