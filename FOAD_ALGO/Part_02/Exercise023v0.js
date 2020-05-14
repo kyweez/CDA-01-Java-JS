01116ee2a482d46dfc9c3067b474ebb715c220fc
kyweez <auboudier.pro@gmail.com>
2020-05-14 23:57:47 +0200

    Exercise 2.3.0 - Fork Game Program - Add comments

--- FOAD_ALGO/Part_02/Exercise023v0.js
+++ FOAD_ALGO/Part_02/Exercise023v0.js
@@ -1,5 +1,16 @@
+/**
+ * The readline module provides an interface for reading data from a Readable stream (such as process.stdin) one line at a time.
+ */
 const readline = require('readline');
 
+/**
+ * query <string> A statement or query to write to output, prepended to the prompt.
+ * callback <Function> A callback function that is invoked with the user's input in response to the query.
+ * 
+ * The rl.question() method displays the query by writing it to the output.
+ * It waits for user input to be provided on input, then invokes the callback function passing the provided input as the first argument.
+ * When called, rl.question() will resume the input stream if it has been paused.
+ */
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
@@ -29,6 +40,7 @@ function guessMyNumber(){
 	console.log(`=========================`);
 	console.log(`### Fork game program ###`);
 	console.log(`=========================`);
+	// Math.random returns a number beetween 0 and 1. I mutltiply by 100 and round it.
 	var randomNumber = Math.round(Math.random() * (100));
 	var count = 0;
 	askNumber(randomNumber, count);
