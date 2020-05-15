ebf0e5aa88c2f9e67769e8818bbbf216c8bb6cf3
kyweez <auboudier.pro@gmail.com>
2020-05-15 23:53:39 +0200

    Exercise 2.5.0 - 0/2 Game - Init

---
+++ FOAD_ALGO/Part_02/Exercise025v0.js
@@ -0,0 +1,10 @@
+/**
+ * The computer and the player choose a number that can only take 3 values: 0, 1 or 2.
+ * The the computer will simulate its choice by generation of a random number.
+ * If the difference between the numbers chosen is:
+ *      ▪ 2: the player who proposed the highest number wins a point.
+ *      ▪ 1: the player who proposed the lowest number wins a point.
+ *      ▪ 0: no points are scored.
+ * The game ends when one of the two players (the computer or the human player) scored 10 points
+ * or when the player introduces a negative number which indicates he wants to stop playing.
+ */
