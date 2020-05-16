71f3729a995e99bcc018c6cf6dd9c532417a245b
kyweez <auboudier.pro@gmail.com>
2020-05-16 23:44:15 +0200

    Exercise 2.7.1 - Young people - Init

---
+++ FOAD_ALGO/Part_02/Exercise027v1.js
@@ -0,0 +1,23 @@
+/**
+* It is a question of counting all people of age strictly less than 20 years among a sample of 20 people. 
+* The user is prompted to enter the 20 values.
+* Describe the algorithm that displays the number of young people and code the solution. 
+*/
+
+const readline = require(`readline`);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function askAge(){
+    rl.question(`Enter an age`, answer =>{
+        fonction de rappel.
+    });
+}
+
+function main(){
+    askAge();
+}
+
+main();
