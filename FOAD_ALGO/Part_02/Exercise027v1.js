d93776791cc80e6f5dd9c0b435a9e6010dd90caa
kyweez <auboudier.pro@gmail.com>
2020-05-17 01:21:32 +0200

    Exercise 2.7.1 - Young people - Typo

--- FOAD_ALGO/Part_02/Exercise027v1.js
+++ FOAD_ALGO/Part_02/Exercise027v1.js
@@ -1,8 +1,8 @@
 /**
-* It is a question of counting all people of age strictly less than 20 years among a sample of 20 people. 
-* The user is prompted to enter the 20 values.
-* Describe the algorithm that displays the number of young people and code the solution. 
-*/
+ * It is a question of counting all people of age strictly less than 20 years among a sample of 20 people. 
+ * The user is prompted to enter the 20 values.
+ * Describe the algorithm that displays the number of young people and code the solution.
+ */
 
 /**
  * Instanciating readline module
