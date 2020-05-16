a2eabd00aa5a47855e8b7ca65b76b4a074807973
kyweez <auboudier.pro@gmail.com>
2020-05-17 01:23:49 +0200

    Exercise 2.5.0 - 2/0 Game Programe - Typo

--- FOAD_ALGO/Part_02/Exercise025v0.js
+++ FOAD_ALGO/Part_02/Exercise025v0.js
@@ -1,13 +1,13 @@
 /**
-* The computer and the player choose a number that can only take 3 values: 0, 1 or 2.
-* The the computer will simulate its choice by generation of a random number.
-* If the difference between the numbers chosen is:
-*      ▪ 2: the player who proposed the highest number wins a point.
-*      ▪ 1: the player who proposed the lowest number wins a point.
-*      ▪ 0: no points are scored.
-* The game ends when one of the two players (the computer or the human player) scored 10 points
-* or when the player introduces a negative number which indicates he wants to stop playing.
-*/
+ * The computer and the player choose a number that can only take 3 values: 0, 1 or 2.
+ * The the computer will simulate its choice by generation of a random number.
+ * If the difference between the numbers chosen is
+ *       ▪ 2: the player who proposed the highest number wins a point.
+ *       ▪ 1: the player who proposed the lowest number wins a point.
+ *       ▪ 0: no points are scored.
+ * The game ends when one of the two players (the computer or the human player) scored 10 points
+ * or when the player introduces a negative number which indicates he wants to stop playing.
+ */
 
 /**
  * Instanciating readline module
@@ -156,7 +156,7 @@ function init(score) {
 
 function zeroTwoGame() {
     console.log(`===============================`);
-    console.log(`### Zero - Two Game Program ###`);
+    console.log(`### Two - Zero Game Program ###`);
     console.log(`===============================`);
 
     //I had to use a table, because variable are locale, then i can't change it 
