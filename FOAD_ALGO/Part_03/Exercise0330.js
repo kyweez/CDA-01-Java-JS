3e07914d43de1c36c550b0951d0581274a3747fe
kyweez <auboudier.pro@gmail.com>
2020-05-17 17:04:41 +0200

    Exercise 3.3.0 - Number of letters - Init

--- FOAD_ALGO/Part_03/Exercise0330.js
+++ FOAD_ALGO/Part_03/Exercise0330.js
@@ -15,22 +15,42 @@ const rl = readline.createInterface({
 });
 
 //This regex checks if the string has more than a letter.
-const regexLetter = /^[a-zA-Z]{1}$/;
+const regex = /^([A-Z]{1})([a-zA-Z ,'\.]{117,})([a-z\.]{1}[?!\.]{1}$)$/;
 
+function checkLetters(answer){
+    console.log(`\n`);
+    console.log(answer);
+}
 
+function checkInput(answer, string){
+    if (!answer.match(regex) && answer!=""){
+        console.log(`Bad input. Try again and follow the rules...`);
+        asktext(string);
+    }
+    else{
+        if (answer == "")
+            answer = string;
+        checkLetters(answer);
+    }
+}
 
-/**
- * This function asks the user to input a letter he wants to find in given strings
- * @param {*} stringTable 
- */
-function askLetter(stringTable){
-    rl.question(`\nPlease insert a letter you want to find (case sensitive): `, answer => {
-        checkInput(answer, stringTable);
+function asktext(string) {
+    rl.question(`\nPlease insert your text : `, answer => {
+        checkInput(answer, string);
     });
 }
 
 function main() {
+    let string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porttitor nulla id dui tempor pretium. Nunc tincidunt.";
+    console.log(`\n=================================`);
+    console.log(`### FIND THE NUMBER OF LETTERS###`);
+    console.log(`=================================`);
+    console.log(`\nYou have to insert a text. There are 2 rules.`);
+    console.log(`     - The text must be longer than 120 characters`);
+    console.log(`     - The text must end with a dot (. / ? / !)`);
+    console.log(`If nothing is inserted, the program will generate a text.`);
 
+    asktext(string);
 
 }
 
