12f91aca6eb78f7d9108c79482585070438e78fd
kyweez <auboudier.pro@gmail.com>
2020-05-24 22:18:29 +0200

    Exercise 3.7 - Binary search - Starting algo

--- FOAD_ALGO/Part_03/Exercise0370.js
+++ FOAD_ALGO/Part_03/Exercise0370.js
@@ -5,12 +5,46 @@ const rl = readline.createInterface({
 });
 
 // This regex checks if the player 1 inserted a valid word
-const regexPlayer1 = /^[a-zA-Z]{5,12}$/
+const controlRegex = /^[a-zA-Z]+$/
 
 const fs = require(`fs`);
 
+function
 
+//
+function checkInput(answer, sortedTable){
+    if (answer.match(controlRegex)){
+        return (true);
+    }
+    else{
+        console.log(`Bad input... Insert a firstname (letters only)`)
+        return (false);
+    }
+}
 
+/**
+ * This function asks the user to input a firstname.
+ * @param {*} sortedTable 
+ */
+function startGame(sortedTable) {
+    rl.question(`Please input a firstname : `, answer => {
+        if (checkInput(answer, sortedTable)){
+            let result = binarySearch 
+        }
+        else{
+            console.log(`Bad input`);
+            startGame(sortedTable);
+        }
+    })
+}
+
+/**
+ * This function takes 2 arguments. Each argument is a table.
+ * It will fill a new table with sorted values from the 2 tables.
+ * @param {*} firstTable 
+ * @param {*} secondTable
+ * @returns the sorted table
+ */
 function merge(firstTable, secondTable) {
     let sortedTable = [];
     let i = 0;
@@ -32,33 +66,38 @@ function merge(firstTable, secondTable) {
     }
     return (sortedTable);
 }
-
+/**
+ * This recursive function will split the table given as an argument in 2 parts
+ * The first part is stored in a table. The second one is stored on a second table.
+ * Eachj table is given as an argument on the recursive function until we have 2 tables of 1 case.
+ * Each previous splitted table is stacked on a LIFO heap. 
+ * Then the merge function is called for each element of the stack.
+ * @param {*} tableToSort
+ * @returns A sorted table
+ */
 function mergeSort(tableToSort) {
     let tableLength;
     let firstTable;
     let secondTable;
 
     tableLength = tableToSort.length;
+    // This is the stopping condition
     if (tableLength == 1)
         return (tableToSort);
+    // First split
     firstTable = tableToSort.slice(0, tableLength / 2);
     secondTable = tableToSort.slice(tableLength / 2)
-
+    // ### Recursive call ###
+    // Each table will be given the returned table when tableLength == 1
     firstTable = mergeSort(firstTable);
     secondTable = mergeSort(secondTable);
     return (merge(firstTable, secondTable));
 }
 
 function main() {
-    // let stringTxt = fs.readFileSync(`Exercise0370.txt`); 
-
-    // const tableTxt = stringTxt.toString().split("\r\n"); 
-    // console.log(tableTxt);
-    // const sortedTable = sortTable(tableTxt);
-    let tab = [1,2,0,0,3,2,3,1,2,3,1,2,312,3,1,2,31,5,63,2,64,5,6,8,4,3,6,23,4,52,3,67,2,3,4,5,2,35,4,1,2,35,2,465,2];
-    console.log(tab.join(" "));
-    console.log(mergeSort(tab).join(" "));
-    process.exit(0);
-    // console.log(fusionSort(tab));
+    let stringTxt = fs.readFileSync(`Exercise0370.txt`);
+    const tableTxt = stringTxt.toString().split("\r\n");
+    const sortedTable = mergeSort(tableTxt);
+    startGame(sortedTable);
 }
 main();
