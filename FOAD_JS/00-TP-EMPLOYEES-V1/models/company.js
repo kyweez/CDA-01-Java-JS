b3c70b2250590083fc28a645349397974d350701
kyweez <auboudier.pro@gmail.com>
2020-05-31 16:48:13 +0200

    TP-Employees-v1 / Adding comments

--- FOAD_JS/00-TP-EMPLOYEES-V1/models/company.js
+++ FOAD_JS/00-TP-EMPLOYEES-V1/models/company.js
@@ -1,6 +1,10 @@
-
 const Employee = require('./employee.js');
 
+/**
+ * This function returns either true if the _employee match to the test or false
+ * @param Employee _employee
+ * @returns Boolean 
+ */
 function isValidEmployee(_employee) {
     const UPPERCASE = /(^[A-Z]+)([ -]{1})?([A-Z]+$)/;
     const CAPITALIZE = /(^[A-Z]{1}[a-z]{1,})(([ -]{1}[A-Za-z]{1})?([a-z]+$))|(^[A-Z]{1}[a-z]{1,}$)/;
@@ -18,6 +22,12 @@ function isValidEmployee(_employee) {
     return (true);
 }
 
+/**
+ * This function returns an unique number depending of the previous IDs 
+ * @param Object[] _employeeDB 
+ * @param Employee _employee
+ * @returns A number of the new to set 
+ */
 function changeID(_employeeDB, _employee) {
     staffing = _employeeDB.length;
     if (staffing === 0) {
@@ -29,11 +39,23 @@ function changeID(_employeeDB, _employee) {
     }
 }
 
+/**
+ * @class Company
+ */
 class Company {
+
+    /**
+     * @constructor for the Company model
+     */
     constructor() {
         this.employeeDB = [];
     }
 
+    /**
+     * This function creates a new entry in the database
+     * @param Employee _employee
+     * @returns false if an error occurs when creating the new DB entry
+     */
     create(_employee) {
         if (isValidEmployee(_employee)) {
             _employee.id = changeID(this.employeeDB, _employee);
@@ -43,6 +65,12 @@ class Company {
             return (false);
     }
 
+    /**
+     * This function returns the desired Employee object
+     * @param int _id 
+     * @returns1 the Employee Object which has the ID given as an argument
+     * @returns2 false if the ID doesn't exist
+     */
     read(_id) {
         if (!(_id >= 0 && _id < Infinity))
             return (false);
@@ -52,6 +80,12 @@ class Company {
         return (buffer);
     }
 
+    /**
+     * This function updates an entry in the DB. (depending on the the ID of the object given as an argument)
+     * @param Employee _employee
+     * @returns1 Boolean true if it works.
+     * @returns2 Boolean false if the update failed
+     */
     update(_employee) {
         if (!(_employee.id >= 0 && _employee.id < Infinity) || !isValidEmployee(_employee))
             return (false);
@@ -62,6 +96,12 @@ class Company {
         return (true);
     }
 
+    /**
+     * This function deletes an entry in the DB. (depending on the the ID given as an argument)
+     * @param int _id
+     * @returns1 Boolean true if it works.
+     * @returns2 Boolean false if the delete failed
+     */
     delete(_id) {
         if (!(_id >= 0 && _id < Infinity))
             return (false);
@@ -72,6 +112,11 @@ class Company {
         return (true);
     }
 
+    /**
+     * This function returns the database sorted by _filter value
+     * @param string _filter
+     * @returns Object[] sorted by _filter
+     */
     readAll(_filter) {
         return (this.employeeDB.sort((value1, value2) => {
             switch (_filter) {
@@ -93,10 +138,13 @@ class Company {
         }));
     }
 
+    /**
+     * This object literal give an acces to salary features
+     */
     salary = {
-        highest: () => this.readAll('salary')[this.employeeDB.length-1].salary,
+        highest: () => this.readAll('salary')[this.employeeDB.length - 1].salary,
         lowest: () => this.readAll('salary')[0].salary,
-        gap:     () => this.salary.highest() - this.salary.lowest()
+        gap: () => this.salary.highest() - this.salary.lowest()
     }
 }
 
