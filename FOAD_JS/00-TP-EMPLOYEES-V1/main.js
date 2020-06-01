1a747ec101c2369d0355cf8a41bcce5f77f42db9
kyweez <auboudier.pro@gmail.com>
2020-06-01 09:33:19 +0200

    TP-Employees-v1 / Update main

--- FOAD_JS/00-TP-EMPLOYEES-V1/main.js
+++ FOAD_JS/00-TP-EMPLOYEES-V1/main.js
@@ -12,24 +12,25 @@ const Test4 = require(`./tests/test-4`);
 
 
 function launchTest(_answer, tabTest) {
-    
-    tabTest[_answer-1].launch();
+
+    tabTest[_answer - 1].launch();
     doTest(tabTest);
 }
 
 function checkValidInput(_answer, tabTest) {
     console.clear();
     if (!_answer.match(/^[1-4]{1}$|^[qQ]$/))
-        doTest();
-    if (_answer.match(/^[qQ]$/)) {
+        doTest(tabTest);
+    else if (_answer.match(/^[qQ]$/)) {
         RL.close();
         process.exit();
     }
-    launchTest(parseInt(_answer), tabTest);
+    else
+        launchTest(parseInt(_answer), tabTest);
 }
 
 function doTest(tabTest) {
-    let question = "Which test do you want to perform ?\n1 - Test\n2 - Test\n3 - Test\n4 - Test\nQ - End the program\n";
+    let question = "\nWhich test do you want to perform ?\n1 - Test 1\n2 - Test 2\n3 - Test 3\n4 - Test 4\nQ - End the program\n";
     RL.question(question, (answer) => {
         checkValidInput(answer, tabTest);
     });
