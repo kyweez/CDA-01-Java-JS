eecd882a30f20406afca4496953e0eebd4d30ba8
kyweez <auboudier.pro@gmail.com>
2020-06-01 10:42:57 +0200

    TP-Employee-v1 / Test 4 OK

--- FOAD_JS/00-TP-EMPLOYEES-V1/tests/test-4.js
+++ FOAD_JS/00-TP-EMPLOYEES-V1/tests/test-4.js
@@ -1,10 +1,37 @@
+const Employee = require(`./../models/employee.js`);
+const Company = require(`./../models/company.js`);
+const Utils = require(`./../models/utils.js`);
+
 class Test4{
     constructor(){
-        this.title = `################ TEST 4 ################`;
+        this.title = `################################ TEST 4 ################################`;
     }
     launch(){
+        console.log("########################################################################");
         console.log(this.title);
-        console.log(`test4`);
+        console.log("########################################################################\n");
+        
+        console.log(`\x1b[35mCreating 3 employees...\n\x1b[0m`);
+        let company = new Company();
+        company.create(new Employee(123, `Macron`,`Emmanuel`, `President`, 999999, new Date(`2017-05-14`)));
+        company.create(new Employee(123, `Hollande`,`Francois`, `Chomeur`, 220000, new Date(`2012-05-15`)));
+        company.create(new Employee(123, `Sarkozy`,`Nicolas`, `Escroc`, 999999999, new Date(`1955-01-28`)));
+        company.create(new Employee(123, `Melenchon`,`Jean-Luc`, `Clown`, 24000, new Date(`2022-05-14`)));
+        
+        console.log(`\n\x1b[35mTesting the readAll(_filter) function (using firstName):\x1b[0m`);
+        console.log(company.readAll(`firstName`));
+
+        console.log(`\n\x1b[35mTesting the delete(_id) function (using ID 2):\x1b[0m`);
+        console.log("company.delete(2)");
+        company.delete(2);
+        console.log(`\x1b[35mTesting the readAll(_filter) function (using hireDate):\x1b[0m`);
+        console.log(company.readAll(`hireDate`));
+        console.log
+
+        console.log(`\n\x1b[35mTesting the salary functions :\x1b[0m`);
+        console.log(`Highest salary : ${company.salary.highest()}`);
+        console.log(`Lowest salary  : ${company.salary.lowest()}`);
+        console.log(`Salary gap     : ${company.salary.gap()}`);
     }
 }
 
