f4bec949bf8f88a095aea7fa4d8f9eaf33915623
kyweez <auboudier.pro@gmail.com>
2020-06-05 10:27:20 +0200

    #9-TP-Area-v2 / getGrid function updated

--- FOAD_JS/01-TP-POINTS/v0.2/models/Area.js
+++ FOAD_JS/01-TP-POINTS/v0.2/models/Area.js
@@ -9,19 +9,21 @@ class Area {
      * @param int _height
      */
     constructor(_width, _height) {
-        this.width = parseInt(_width);
-        this.height = parseInt(_height);
+        this.gameAreaWidth = parseInt(_width);
+        this.gameAreaHeight = parseInt(_height);
+        this.totalAreaWidth = parseInt(_width);
+        this.totalAreaHeight = parseInt(_height);
         this.totalArea = (() => {
             let i;
             let j;
             let tab1D;
             let tab2D = [];
-            for (i = 0; i < this.height; i++) {
+            for (i = 0; i < this.totalAreaHeight; i++) {
                 tab2D.push((() => {
                     j = 0;
                     tab1D = [];
-                    for (j; j < this.width; j++) {
-                        tab1D.push(`□`);
+                    for (j; j < this.totalAreaWidth; j++) {
+                        tab1D.push(`  `);
                     }
                     return (tab1D);
                 })());
@@ -34,67 +36,45 @@ class Area {
         // On ne peut pas utiliser le for of ici. Car on doit gerer l'aire de jeu a l'interieur de l'aire totale.
         let x;
         let y;
-        let areaHeight = this.totalArea.reduce((count, row) => count + row.length, 0);
-        console.log(areaHeight);
-        let areaWidth = this.totalArea.reduce((count, row) => count + row.length, 0);
         let gridString = "";
 
-        for (y = 0; y< areaHeight; y++){
-            for(x=0; x < areaWidth; x++){
-                if (x < this.height){
-                    if (this.totalArea[y][x] instanceof Point){
-                        gridString += `\x1b[32m█ \x1b[0m`
-                    }
-                    else{
-                        gridString += `\x1b[36m${this.totalArea[y][x]} \x1b[0m`
-                    }
+        for (y = 0; y< this.totalAreaHeight; y++){
+            for(x=0; x < this.totalAreaWidth; x++){
+                if (x < this.gameAreaWidth && y < this.gameAreaHeight){
+                    if (this.totalArea[y][x] instanceof Point)
+                        gridString += `\x1b[42m\x1b[30m  \x1b[0m`
+                    else
+                        gridString += `\x1b[44m${this.totalArea[y][x]}\x1b[0m`
                 }
                 else{
-                    if (this.totalArea[y][x] instanceof Point){
-                        gridString += `\x1b[31m█ \x1b[0m`
-                    }
-                    else{
-                        gridString += `\x1b[34m${this.totalArea[y][x]} \x1b[0m`
-                    }
+                    if (this.totalArea[y][x] instanceof Point)
+                        gridString += `\x1b[41m\x1b[30m  \x1b[0m`
+                    else
+                        gridString += `\x1b[46m${this.totalArea[y][x]}\x1b[0m`
                 }
             }
             gridString += `\n`;
         }
         return (gridString);
     }
-
-    // getTotalGrid() {
-    //     let i;
-    //     let length;
-    //     let grid = "";
-
-    //     for (const tab of this.totalArea) {
-    //         i = 0;
-    //         length = tab.length;
-    //         for (i; i < length; i++) {
-    //             if (tab[i] instanceof Area){
-    //                 if (){
-
-    //                 }
-    //                 else
-    //                 grid += `\x1b[31m█ \x1b[0m`;
-
-    //             }
-    //             else
-    //                 grid += `\x1b[34m${tab[i]} \x1b[0m`;
-    //         }
-    //         grid += `\n`;
-    //     }
-    //     return (grid);
-    // }
-
 }
 
-let area = new Area(2, 2);
-// let j = 30;
-// for (let i = 0; i < 31; i++) {
-//     area.totalArea[i][i] = new Point(i, i);
-//     area.totalArea[i][j] = new Point(i, i);
-//     j--;
-// }
+let area = new Area(31, 31);
+area.gameAreaHeight = 21;
+area.gameAreaWidth = 21;
+let j = 30;
+for (let i = 0; i < 31; i++) {
+    area.totalArea[i][i] = new Point(i, i);
+    area.totalArea[i][j] = new Point(i, i);
+    j--;
+}
+// area.totalAreaHeight = 35;
+// area.totalAreaWidth = 38;
+// area.totalArea[37][34] = new Point(37, 34);
 console.log(area.getGrid());
+
+// let area = new Area(2, 4);
+// for (let i = 0; i < 2; i++) {
+//     area.totalArea[i][i] = new Point(i, i);
+// }
+// console.log(area.getGrid());
