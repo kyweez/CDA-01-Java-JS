67fd2a71c0f327fd1cd6e0a8ec469cb61a47bc61
kyweez <auboudier.pro@gmail.com>
2020-06-05 21:50:26 +0200

    #9-TP-Area-v2 / addPoint function OK

--- FOAD_JS/01-TP-POINTS/v0.2/models/Area.js
+++ FOAD_JS/01-TP-POINTS/v0.2/models/Area.js
@@ -1,4 +1,5 @@
 const Point = require(`./Point.js`);
+const SYMBOL = `  `;
 
 class Area {
 
@@ -23,7 +24,7 @@ class Area {
                     j = 0;
                     tab1D = [];
                     for (j; j < this.totalAreaWidth; j++) {
-                        tab1D.push(`  `);
+                        tab1D.push(SYMBOL);
                     }
                     return (tab1D);
                 })());
@@ -32,23 +33,81 @@ class Area {
         })();
     }
 
+    pushOrdinate(tab2D, y) {
+        let abscissaLength = tab2D[0].length;
+        for (let i = 0; i < y; i++) {
+            let tab1D = [];
+            tab2D.push((() => {
+                for (let j = 0; j < abscissaLength; j++)
+                    tab1D.push(SYMBOL);
+                return (tab1D);
+            })());
+        }
+        return (tab2D);
+    }
+
+    pushAbscissa(tab2D, x) {
+        let ordinateLength = tab2D.reduce((count, row) => (count + row.length / row.length), 0);
+        for (let i = 0; i < ordinateLength; i++) {
+            for (let j = 0; j < x; j++)
+                tab2D[i].push(SYMBOL);
+        }
+        return (tab2D);
+    }
+
+    /**
+     * This function add a Point in the area
+     * Le "Point" peut se trouver hors des limites de la zone
+     * @param Point _point 
+     * @returns Boolean true en cas de succès, false si l'ajout est impossible 
+     */
+    addPoint(_point) {
+        if (!(_point instanceof Point))
+            return (false);
+        if (_point.x < this.totalAreaWidth && _point.y < this.totalAreaHeight) {
+            if (this.totalArea[_point.y][_point.x] instanceof Point)
+                return (false);
+            this.totalArea[_point.y][_point.x] = _point;
+            return (true);
+        }
+        else {
+            if (_point.x >= this.totalAreaWidth) {
+                let diff = _point.x - this.totalAreaWidth + 1;
+                this.totalArea = this.pushAbscissa(this.totalArea, diff);
+                this.totalAreaWidth = _point.x + 1;
+            }
+            if (_point.y >= this.totalAreaHeight) {
+                let diff = _point.y - this.totalAreaHeight + 1;
+                this.totalArea = this.pushOrdinate(this.totalArea, diff);
+                this.totalAreaHeight = _point.y + 1;
+            }
+        }
+        this.totalArea[_point.y][_point.x] = _point;
+        return (true);
+    }
+
+    /**
+     * This function returns the full grid. There is 2 areas, the game area included in the total area.
+     * Point outside the game area are colored red
+     * @returns string of the grid.
+     */
     getGrid() {
         // On ne peut pas utiliser le for of ici. Car on doit gerer l'aire de jeu a l'interieur de l'aire totale.
         let x;
         let y;
         let gridString = "";
 
-        for (y = 0; y< this.totalAreaHeight; y++){
-            for(x=0; x < this.totalAreaWidth; x++){
-                if (x < this.gameAreaWidth && y < this.gameAreaHeight){
+        for (y = 0; y < this.totalAreaHeight; y++) {
+            for (x = 0; x < this.totalAreaWidth; x++) {
+                if (x < this.gameAreaWidth && y < this.gameAreaHeight) {
                     if (this.totalArea[y][x] instanceof Point)
-                        gridString += `\x1b[42m\x1b[30m  \x1b[0m`
+                        gridString += `\x1b[47m\x1b[30m${SYMBOL}\x1b[0m`
                     else
                         gridString += `\x1b[44m${this.totalArea[y][x]}\x1b[0m`
                 }
-                else{
+                else {
                     if (this.totalArea[y][x] instanceof Point)
-                        gridString += `\x1b[41m\x1b[30m  \x1b[0m`
+                        gridString += `\x1b[41m\x1b[30m${SYMBOL}\x1b[0m`
                     else
                         gridString += `\x1b[46m${this.totalArea[y][x]}\x1b[0m`
                 }
@@ -59,22 +118,11 @@ class Area {
     }
 }
 
-let area = new Area(31, 31);
-area.gameAreaHeight = 21;
-area.gameAreaWidth = 21;
-let j = 30;
-for (let i = 0; i < 31; i++) {
-    area.totalArea[i][i] = new Point(i, i);
-    area.totalArea[i][j] = new Point(i, i);
-    j--;
-}
-// area.totalAreaHeight = 35;
-// area.totalAreaWidth = 38;
-// area.totalArea[37][34] = new Point(37, 34);
+let area = new Area(5, 5);
+area.addPoint(new Point(4, 4));
+area.addPoint(new Point(4, 4));
+area.addPoint(new Point(5, 5));
+area.addPoint(new Point(5, 5));
+area.addPoint(new Point(6, 6));
+area.addPoint(new Point(7, 7));
 console.log(area.getGrid());
-
-// let area = new Area(2, 4);
-// for (let i = 0; i < 2; i++) {
-//     area.totalArea[i][i] = new Point(i, i);
-// }
-// console.log(area.getGrid());
