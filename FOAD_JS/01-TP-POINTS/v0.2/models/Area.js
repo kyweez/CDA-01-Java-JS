79cba59cb946d6c0e199d394932bce47ed68e439
kyweez <auboudier.pro@gmail.com>
2020-06-06 14:54:44 +0200

    #9-TP-Area-v2 / checkMaxBound function OK

--- FOAD_JS/01-TP-POINTS/v0.2/models/Area.js
+++ FOAD_JS/01-TP-POINTS/v0.2/models/Area.js
@@ -33,39 +33,6 @@ class Area {
         })();
     }
 
-    /**
-     * This function returns the new tab[][] with "y" more ordinates 
-     * @param Object[] tab2D 
-     * @param int y
-     * @returns tab2D
-     */
-    pushOrdinate(tab2D, y) {
-        let abscissaLength = tab2D[0].length;
-        for (let i = 0; i < y; i++) {
-            let tab1D = [];
-            tab2D.push((() => {
-                for (let j = 0; j < abscissaLength; j++)
-                    tab1D.push(SYMBOL);
-                return (tab1D);
-            })());
-        }
-        return (tab2D);
-    }
-
-    /**
-     * This function returns the new tab[][] with "x" more abscissas 
-     * @param Object[] tab2D 
-     * @param int x
-     * @returns tab2D
-     */
-    pushAbscissa(tab2D, x) {
-        let ordinateLength = tab2D.reduce((count, row) => (count + row.length / row.length), 0);
-        for (let i = 0; i < ordinateLength; i++) {
-            for (let j = 0; j < x; j++)
-                tab2D[i].push(SYMBOL);
-        }
-        return (tab2D);
-    }
 
     /**
      * This function add a Point in the area
@@ -112,20 +79,112 @@ class Area {
             return (false);
         if (this.totalArea[_point.y][_point.x] !== _point)
             return false;
-        if (abscissa < this.totalAreaWidth && ordinate< this.totalAreaHeight){
+        if (abscissa < this.totalAreaWidth && ordinate < this.totalAreaHeight) {
             if (this.totalArea[ordinate][abscissa] instanceof Point)
                 return (false);
             this.totalArea[ordinate][abscissa] = _point;
             this.totalArea[_point.y][_point.x] = SYMBOL;
             _point.x = abscissa;
             _point.y = ordinate;
+            // TODO supprimer repere si dernier point du repere
             return (true);
         }
-        else{
-            //TODO 
+        else {
+            if (abscissa >= this.totalAreaWidth) {
+                let diff = abscissa - this.totalAreaWidth + 1;
+                this.totalArea = this.pushAbscissa(this.totalArea, diff);
+                this.totalAreaWidth = abscissa + 1;
+            }
+            if (ordinate >= this.totalAreaHeight) {
+                let diff = ordinate - this.totalAreaHeight + 1;
+                this.totalArea = this.pushOrdinate(this.totalArea, diff);
+                this.totalAreaHeight = ordinate + 1;
+            }
+            this.totalArea[ordinate][abscissa] = _point;
+            this.totalArea[_point.y][_point.x] = SYMBOL;
+            _point.x = abscissa;
+            _point.y = ordinate;
+            return (true);
         }
 
     }
+
+    /**
+     * This function returns the new tab[][] with "y" more ordinates 
+     * @param Object[] tab2D 
+     * @param int y
+     * @returns tab2D
+     */
+    pushOrdinate(tab2D, y) {
+        let abscissaLength = tab2D[0].length;
+        for (let i = 0; i < y; i++) {
+            let tab1D = [];
+            tab2D.push((() => {
+                for (let j = 0; j < abscissaLength; j++)
+                    tab1D.push(SYMBOL);
+                return (tab1D);
+            })());
+        }
+        return (tab2D);
+    }
+
+    /**
+     * This function returns the new tab[][] with "x" more abscissas 
+     * @param Object[] tab2D 
+     * @param int x
+     * @returns tab2D
+     */
+    pushAbscissa(tab2D, x) {
+        let ordinateLength = tab2D.reduce((count, row) => (count + row.length / row.length), 0);
+        for (let i = 0; i < ordinateLength; i++) {
+            for (let j = 0; j < x; j++)
+                tab2D[i].push(SYMBOL);
+        }
+        return (tab2D);
+    }
+
+    checkMaxBound() {
+        let maxPoint = new Point();
+        let i = 0;
+        let isPoint = false;
+        let maxAbscissa = this.totalAreaWidth - 1;
+        let maxOrdinate = this.totalAreaHeight - 1;
+
+
+        while (!isPoint) {
+            i = 0;
+            while (i < this.totalAreaHeight && !isPoint) {
+                if (this.totalArea[i][maxAbscissa] instanceof Point) {
+                    maxPoint.x = maxAbscissa;
+                    isPoint = true;
+                }
+                else if (i === this.totalAreaHeight - 1) {
+                    maxAbscissa--;
+                    i++
+                }
+                else
+                    i++;
+            }
+        }
+        isPoint = false;
+        while (!isPoint) {
+            i = 0;
+            while (i < this.totalAreaWidth && !isPoint) {
+                if (this.totalArea[maxOrdinate][i] instanceof Point) {
+                    maxPoint.y = maxOrdinate;
+                    isPoint = true;
+                }
+                else if (i === this.totalAreaWidth - 1) {
+                    maxOrdinate--;
+                    i++
+                }
+                else
+                    i++;
+            }
+        }
+        return (maxPoint);
+    }
+
     /**
      * This function returns the full grid. There is 2 areas, the game area included in the total area.
      * Point outside the game area are colored red
@@ -166,3 +225,16 @@ area.addPoint(new Point(5, 5));
 area.addPoint(new Point(6, 6));
 area.addPoint(new Point(7, 7));
 console.log(area.getGrid());
+// area.movePoint(area.totalArea[4][4], 0, 0);
+// console.log(area.getGrid());
+let point1 = new Point(8, 0);
+area.addPoint(point1);
+console.log(area.getGrid());
+area.movePoint(point1, 10, 10);
+console.log(area.getGrid());
+area.movePoint(point1, 8, 8);
+console.log(area.getGrid());
+console.log(area.checkMaxBound());
+area.movePoint(point1, 12, 12);
+console.log(area.getGrid());
+console.log(area.checkMaxBound());
