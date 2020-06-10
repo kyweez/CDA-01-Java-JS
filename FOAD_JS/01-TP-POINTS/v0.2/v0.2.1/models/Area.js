43653761019970435e02295c8999e3ec5fb96bb1
kyweez <auboudier.pro@gmail.com>
2020-06-10 21:51:01 +0200

    #9-Area-v2.1 / Constructor + addPoint OK

--- FOAD_JS/01-TP-POINTS/v0.2/v0.2.1/models/Area.js
+++ FOAD_JS/01-TP-POINTS/v0.2/v0.2.1/models/Area.js
@@ -1,249 +1,109 @@
 const Point = require(`./Point.js`);
-const SYMBOL = `  `;
 
 class Area {
-    /** Origine instanciation : private because we musn't move this point **/
-    #origine = new Point(0,0);
-
     /**
-     * @constructor Initialize a new instance of the "Area" class
-     * The width and the height respectively define the number of columns and rows.
-     * @param int _width
-     * @param int _height
+     * Constructeur: Initialise une nouvelle instance de la classe "Area"
+     * La largeur et la hauteur définissent respectivement le nombre de colonnes et de lignes. 
+     * @param int _width largeur de la zone
+     * @param int _height hauteur de la zone
      */
     constructor(_width, _height) {
-        this.gameAreaWidth = parseInt(_width);
-        this.gameAreaHeight = parseInt(_height);
-        this.totalAreaWidth = parseInt(_width);
-        this.totalAreaHeight = parseInt(_height);
-        this.totalArea = (() => {
-            let i;
-            let j;
-            let tab1D;
-            let tab2D = [];
-            for (i = 0; i < this.totalAreaHeight; i++) {
-                tab2D.push((() => {
-                    j = 0;
-                    tab1D = [];
-                    for (j; j < this.totalAreaWidth; j++) {
-                        tab1D.push(SYMBOL);
-                    }
-                    return (tab1D);
-                })());
-            }
-            return (tab2D);
-        })();
-        this.totalArea[0][0] = this.getOrigine();
-    }
-
-    getOrigine(){
-        return(this.#origine);
+        this.width = parseInt(_width || 1);
+        this.height = parseInt(_height || 1);
+        this.tab = [new Point(0, 0)];
+        this.tabSize = this.width * this.height;
+        this.freeCellsTable = this.freeCellsCalcul();
     }
 
     /**
-     * This function add a Point in the area
-     * The point can be outside of the game area. If so, the totalArea is increased.
-     * If the point already exists, returns false.
-     * If the point can be set, put the point into the corresponding cell.
+     * Ajoute un "Point" dans la zone
+     * Le "Point" peut se trouver hors des limites de la zone
      * @param Point _point 
-     * @returns Boolean true or false
+     * @returns Boolean true en cas de succès, false si l'ajout est impossible 
      */
     addPoint(_point) {
         if (!(_point instanceof Point))
             return (false);
-        if (_point.x < this.totalAreaWidth && _point.y < this.totalAreaHeight) {
-            if (this.totalArea[_point.y][_point.x] instanceof Point)
-                return (false);
-            this.totalArea[_point.y][_point.x] = _point;
-            return (true);
-        }
-        else {
-            if (_point.x >= this.totalAreaWidth) {
-                let diff = _point.x - this.totalAreaWidth + 1;
-                this.totalArea = this.pushAbscissa(this.totalArea, diff);
-                this.totalAreaWidth = _point.x + 1;
-            }
-            if (_point.y >= this.totalAreaHeight) {
-                let diff = _point.y - this.totalAreaHeight + 1;
-                this.totalArea = this.pushOrdinate(this.totalArea, diff);
-                this.totalAreaHeight = _point.y + 1;
-            }
-        }
-        this.totalArea[_point.y][_point.x] = _point;
+        if (this.tab.length >= this.tabSize)
+            return (false);
+        if (!this.isFreeCell(_point))
+            _point.copy(this.freeCellsTable[0]);
+        this.tab.push(_point);
+        this.tabSize++;
+        this.updateFreeCellsTable(_point);
+        return true;
+    }
+
+    isFreeCell(_point) {
+        if (!(_point instanceof Point))
+            return (false);
+        if ((this.tab.find(test => test.x === _point.x && test.y === _point.y)) !== undefined)
+            return (false);
         return (true);
     }
 
-    /**
-     * Move a point to another coordinates
-     * The new coordinates ccan be outside of the gameArea
-     * @param int abscissa 
-     * @param int ordinate
-     * @returns Boolean true or false
-     */
-    movePoint(_point, abscissa, ordinate) {
-        if (!(_point instanceof Point))
-            return (false);
-        if (this.totalArea[_point.y][_point.x] !== _point)
-            return false;
-        if (abscissa < this.totalAreaWidth && ordinate < this.totalAreaHeight) {
-            if (this.totalArea[ordinate][abscissa] instanceof Point)
-                return (false);
-            this.totalArea[ordinate][abscissa] = _point;
-            this.totalArea[_point.y][_point.x] = SYMBOL;
-            _point.x = abscissa;
-            _point.y = ordinate;
-            this.refactorTotalArea(this.checkMaxBound());
-            return (true);
-        }
-        else {
-            if (abscissa >= this.totalAreaWidth) {
-                let diff = abscissa - this.totalAreaWidth + 1;
-                this.totalArea = this.pushAbscissa(this.totalArea, diff);
-                this.totalAreaWidth = abscissa + 1;
-            }
-            if (ordinate >= this.totalAreaHeight) {
-                let diff = ordinate - this.totalAreaHeight + 1;
-                this.totalArea = this.pushOrdinate(this.totalArea, diff);
-                this.totalAreaHeight = ordinate + 1;
-            }
-            this.totalArea[ordinate][abscissa] = _point;
-            this.totalArea[_point.y][_point.x] = SYMBOL;
-            _point.x = abscissa;
-            _point.y = ordinate;
-            return (true);
-        }
-
-    }
-
-    /**
-     * This function returns the new tab[][] with "y" more ordinates 
-     * @param Object[] tab2D 
-     * @param int y
-     * @returns tab2D
-     */
-    pushOrdinate(tab2D, y) {
-        let abscissaLength = tab2D[0].length;
-        for (let i = 0; i < y; i++) {
-            let tab1D = [];
-            tab2D.push((() => {
-                for (let j = 0; j < abscissaLength; j++)
-                    tab1D.push(SYMBOL);
-                return (tab1D);
-            })());
-        }
-        return (tab2D);
-    }
-
-    /**
-     * This function returns the new tab[][] with "x" more abscissas 
-     * @param Object[] tab2D 
-     * @param int x
-     * @returns tab2D
-     */
-    pushAbscissa(tab2D, x) {
-        let ordinateLength = tab2D.reduce((count, row) => (count + row.length / row.length), 0);
-        for (let i = 0; i < ordinateLength; i++) {
-            for (let j = 0; j < x; j++)
-                tab2D[i].push(SYMBOL);
-        }
-        return (tab2D);
-    }
-
-    /**
-     * This function checks the max bound of abscissa and ordinate.
-     * @returns A theorical point (max_X, max_Y)
-     */
-    checkMaxBound() {
-        let maxPoint = new Point();
-        let i = 0;
-        let isPoint = false;
-        let maxAbscissa = this.totalAreaWidth - 1;
-        let maxOrdinate = this.totalAreaHeight - 1;
-        while (!isPoint) {
-            i = 0;
-            while (i < this.totalAreaHeight && !isPoint) {
-                if (this.totalArea[i][maxAbscissa] instanceof Point) {
-                    maxPoint.x = maxAbscissa;
-                    isPoint = true;
-                }
-                else if (i === this.totalAreaHeight - 1) {
-                    maxAbscissa--;
-                    i++
-                }
-                else
-                    i++;
-            }
-        }
-        isPoint = false;
-        while (!isPoint) {
-            i = 0;
-            while (i < this.totalAreaWidth && !isPoint) {
-                if (this.totalArea[maxOrdinate][i] instanceof Point) {
-                    maxPoint.y = maxOrdinate;
-                    isPoint = true;
-                }
-                else if (i === this.totalAreaWidth - 1) {
-                    maxOrdinate--;
-                    i++
-                }
-                else
-                    i++;
-            }
-        }
-        return (maxPoint);
-    }
-
-    /**
-     * This function cuts the totalArea depending of the Point given as parameter
-     * @param Point _point
-     * @returns True if everything went well, false if something failed.
-     */
-    refactorTotalArea(_point) {
-        if (!(_point instanceof Point))
-            return (false);
-        if (_point.x < this.gameAreaWidth)
-            (_point.x = this.gameAreaWidth-1);
-        if (_point.y < this.gameAreaHeight)
-            (_point.y = this.gameAreaHeight-1);
-        for (let i = 0; i <= _point.y; i++)
-            this.totalArea[i] = this.totalArea[i].splice(0, _point.x + 1);
-        this.totalArea = this.totalArea.splice(0, _point.y + 1)
-        this.totalAreaHeight = _point.y + 1;
-        this.totalAreaWidth = _point.x + 1;
-        return (true);
-    }
-
-    /**
-     * This function returns the full grid. There is 2 areas, the game area included in the total area.
-     * Point outside the game area are colored red
-     * @returns string of the grid.
-     */
-    getGrid() {
-        // On ne peut pas utiliser le for of ici. Car on doit gerer l'aire de jeu a l'interieur de l'aire totale.
+    freeCellsCalcul() {
+        let freeCells = [];
         let x;
         let y;
-        let gridString = "";
-
-        for (y = 0; y < this.totalAreaHeight; y++) {
-            for (x = 0; x < this.totalAreaWidth; x++) {
-                if (x < this.gameAreaWidth && y < this.gameAreaHeight) {
-                    if (this.totalArea[y][x] instanceof Point)
-                        gridString += `\x1b[47m\x1b[30m${SYMBOL}\x1b[0m`
-                    else
-                        gridString += `\x1b[44m${this.totalArea[y][x]}\x1b[0m`
-                }
-                else {
-                    if (this.totalArea[y][x] instanceof Point)
-                        gridString += `\x1b[41m\x1b[30m${SYMBOL}\x1b[0m`
-                    else
-                        gridString += `\x1b[46m${this.totalArea[y][x]}\x1b[0m`
+        let maxFreeCells = this.tabSize - this.tab.length;
+        for (y = 0; y < this.height; y++) {
+            for (x = 0; x < this.width; x++) {
+                if (this.isFreeCell(new Point(x, y))) {
+                    freeCells.push(new Point(x, y));
+                    maxFreeCells--;
+                    if (maxFreeCells === 0)
+                        return (freeCells);
                 }
             }
-            gridString += `\n`;
         }
-        return (gridString);
+        return (freeCells);
+    }
+
+    updateFreeCellsTable(_point) {
+        if (!(_point instanceof Point))
+            return (false);
+        let index = this.freeCellsTable.findIndex(test => test.x === _point.x && test.y === _point.y);
+        this.freeCellsTable.splice(index, 1);
+        return (true);
+    }
+
+    /**
+     * Déplace un point existant dans la zone vers de nouvelles coordonnées
+     * Les nouvelles coordonnées peuvent se trouver hors limites
+     * @returns Boolean true en cas de succès, false en cas d'échec
+     */
+    movePoint() {
+        // implémenter la méthode
+    }
+
+
+    /**
+     * Vérifie la position de chaque "Point" existant dans la zone
+     * Chaque Point hors des limites est automatiquement déplacé dans les limites vers la position libre la plus proche
+     * @returns int le nombre de points déplacés
+     */
+    needAllInside(/* déterminer les paramètres */) {
+        // implémenter la méthode
     }
 }
 
-let area = new Area(4, 4);
-console.log(area.getGrid());
+let area = new Area(3, 3);
+console.log(area);
+let point1 = new Point(1, 1);
+area.addPoint(point1);
+console.log(area);
+
+// let point2 = new Point(1, 0);
+// let point3 = new Point(0, 2);
+// let point4 = new Point(0, 1);
+// let point5 = new Point(2, 2);
+// let point6 = new Point(-1, 1);
+// area.tab.push(point1);
+// area.tab.push(point2);
+// area.tab.push(point3);
+// area.tab.push(point4);
+// area.tab.push(point5);
+// area.tab.push(point6);
+// console.log(area.tab);
+
